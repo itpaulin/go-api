d3a748058bda3ebc9709c371dc848b3f2802ce33
Paulo Ricardo Malta Leal <paulo.leal@wisedit.app>
2026-02-07 23:21:58 -0300

    feat: add instance of server

---
+++ cmd/go-api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/itpaulin/go-api/configs"
+	"github.com/itpaulin/go-api/internal/entity"
+	"github.com/itpaulin/go-api/internal/infra/database"
+	"github.com/itpaulin/go-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	config, err := configs.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+	println("Server running on port:", config.WebServerPort)
+
+	db, err := gorm.Open(sqlite.Open(config.DBPath), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&entity.Product{}, &entity.User{})
+	if err != nil {
+		panic(err)
+	}
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Get("/products", productHandler.GetProducts)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	r.Post("/users", userHandler.CreateUser)
+
+	// http.HandleFunc("/products", productHandler.CreateProduct)
+	err = http.ListenAndServe(":3000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
