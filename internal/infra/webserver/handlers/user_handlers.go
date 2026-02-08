99e89c8a6df446ac4fb318bae5472ab3d9168237
Paulo Ricardo Malta Leal <paulo.leal@wisedit.app>
2026-02-07 23:21:44 -0300

    feat: add user handler

---
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/itpaulin/go-api/internal/dto"
+	"github.com/itpaulin/go-api/internal/entity"
+	"github.com/itpaulin/go-api/internal/infra/database"
+)
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(db database.UserInterface) *UserHandler {
+	return &UserHandler{
+		UserDB: db,
+	}
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
